5885394997f0d1b152585521098ba7f0e916b6c5
josephjoarell@gmail.com <josephjoarell@gmail.com>
2021-09-01 01:15:08 -0300

    libft.h, updated-norminette.

--- libft.h
+++ libft.h
@@ -1,8 +1,10 @@
-#ifndef __LIBFT_H__
-#define __LIBFT_H__
+#ifndef LIBFT_H
+# define LIBFT_H
 
-int ft_isalpha (int c);
-int ft_isdigit (int c);
-int ft_isalnum (int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 #endif
