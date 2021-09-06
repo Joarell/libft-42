53508bc2b45684739c723fbd32f661eb1a73ec62
Joarell <josephjoarell@gmail.com>
2021-09-06 17:09:27 -0300

    libft.h updated!

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 23:07:10 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/06 15:58:05 by Jev              ###   ########.fr       */
+/*   Updated: 2021/09/06 16:01:26 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *s);
 void	*ft_memset(void *dest, int c, int len);
-void	bzero(void *s, int n);
+void	ft_bzero(void *s, int n);
 
 #endif
