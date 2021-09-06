404a563e4ccb126247c692b97a6acf991db0cf2f
Joarell <josephjoarell@gmail.com>
2021-09-05 23:34:19 -0300

    ft_memset, done!

--- ft_memset.c
+++ ft_memset.c
@@ -6,17 +6,18 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 02:44:53 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/05 22:51:23 by Jev              ###   ########.fr       */
+/*   Updated: 2021/09/05 23:12:01 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    *ft_memset(void *dest, int c, int len)
+void	*ft_memset(void *dest, int c, char len)
 {
-    while (len > 0)
-    {
-        *(char *) dest = (char) c;
-        dest++;
-        len--;
-    }
-    return(0);
+
+	while ((unsigned char) len > 0)
+	{
+		*(char *) dest = (char) c;
+		dest++;
+		len--;
+	}
+	return (0);
 }
