4f51ef5966faf4294db5f544b56943bcbe6d6d1d
Joarell <josephjoarell@gmail.com>
2021-09-07 15:16:11 -0300

    libft.h, updated!

--- libft.h
+++ libft.h
@@ -6,12 +6,14 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 23:07:10 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/06 16:01:26 by Jev              ###   ########.fr       */
+/*   Updated: 2021/09/07 15:05:23 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+#include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -19,7 +21,8 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *s);
-void	*ft_memset(void *dest, int c, int len);
+void	*ft_memset(void *dest, int c, size_t len);
 void	ft_bzero(void *s, int n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
