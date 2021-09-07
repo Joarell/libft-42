a326aae0bfb34a7b15cf4b9820db901aa732c15a
Joarell <josephjoarell@gmail.com>
2021-09-07 15:17:31 -0300

    ft_memset.c, done!

--- ft_memset.c
+++ ft_memset.c
@@ -6,11 +6,13 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 02:44:53 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/05 23:36:58 by Jev              ###   ########.fr       */
+/*   Updated: 2021/09/07 12:10:09 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *dest, int c, char len)
+#include "libft.h"
+
+void	*ft_memset(void *dest, int c, size_t len)
 {
 	while ((unsigned char) len > 0)
 	{
