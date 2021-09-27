a32af6e7f19174f7335b4140c64891cdf5a1e55a
Joarell <josephjoarell@gmail.com>
2021-09-27 00:19:06 -0300

    ft_isdigit, done!

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 19:18:05 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/19 17:05:23 by Jev              ###   ########.fr       */
+/*   Updated: 2021/09/27 00:18:43 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,24 @@
 int	ft_atoi(const char *str)
 {
 	int	sign;
-	int	aux;
+	int	i;
 
 	sign = 1;
+	i = 0;
 	if (*str == 0 || *str == '\0')
 		return (0);
 	if (*str == '-')
-	{
 		sign = -1;
-	}
 	str++;
-	if (ft_isdigit(*str) == 0)
-		return (0);
-	aux = 0;
-	while (*str != '\0')
+	i = 0;
+	while (*str != '\0' && ft_isalpha(*str) == 0)
 	{
 		if (ft_isdigit(*str) == 1)
 		{
-			aux *= 10;
-			aux += (*str - '0') * sign;
+			i *= 10;
+			i += (*str - '0') * sign;
 		}
 		str++;
 	}
-	return (aux);
+	return (i);
 }
