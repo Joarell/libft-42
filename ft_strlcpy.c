20ce9f646a46694e2af219a691a40ebd3c89823b
Joarell <josephjoarell@gmail.com>
2021-09-26 21:39:00 -0300

    ft_strlcpy, done!

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 22:48:50 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/26 15:08:03 by Jev              ###   ########.fr       */
+/*   Updated: 2021/09/26 21:38:47 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,4 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	dst++;
 	src++;
 	return (ft_strlcpy(dst, src, size - 1));
-}
-
-
-#include <bsd/string.h>
-#include <stdio.h>
-int main(void)
-{
-	char A[15] = "Balão voando.";
-	char B[14] = "Pato na lagoa";
-	
-	printf("%s \n", A);
-	strlcpy(A, B, 15 * sizeof(char)); 
-	printf("%s \n", A);
-	return (0);
 }
