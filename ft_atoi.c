a78c4358dc2fa429643dd51db86ed88d1677f644
Joarell <josephjoarell@gmail.com>
2021-10-04 22:06:34 -0300

    ft_atoi, done!

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 19:18:05 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/27 00:18:43 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/04 22:05:27 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,22 @@ int	ft_atoi(const char *str)
 
 	sign = 1;
 	i = 0;
+	while (*str <= 32 && *str != '\e')
+		str++;
 	if (*str == 0 || *str == '\0')
 		return (0);
 	if (*str == '-')
-		sign = -1;
-	str++;
-	i = 0;
-	while (*str != '\0' && ft_isalpha(*str) == 0)
 	{
-		if (ft_isdigit(*str) == 1)
-		{
-			i *= 10;
-			i += (*str - '0') * sign;
-		}
+		str++;
+		sign = -1;
+	}
+	if (*str == '+' && sign > 0)
+		str++;
+	i = 0;
+	while (*str != '\0' && ft_isdigit(*str) == 1)
+	{
+		i *= 10;
+		i += (*str - '0') * sign;
 		str++;
 	}
 	return (i);
