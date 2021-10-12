a4664aefbc9b7b1e149239f9e330663dbaa787d0
Joarell <josephjoarell@gmail.com>
2021-10-12 20:40:33 -0300

    ft_strtrim, done!

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,36 +6,77 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 23:15:50 by Jev               #+#    #+#             */
-/*   Updated: 2021/09/26 20:44:52 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/12 20:28:11 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static	void	*start_trim(const char *str, const char *cut)
+{
+	char	*aux;
+	int		len;
+
+	len = ft_strlen(str) - ft_strlen(cut);
+	aux = malloc (len * sizeof(char));
+	if (aux == NULL)
+		return (NULL);
+	len = 0;
+	while (*str != '\0')
+	{
+		if (*str == *cut)
+		{
+			cut++;
+			str++;
+		}
+		if (*str != *cut)
+		{
+			aux[len] = *str;
+			str++;
+			len++;
+		}
+	}
+	return (aux);
+}
+
+static	void	*end_trim(const char *str, const char *cut)
+{
+	char	*aux2;
+	int		len;
+
+	len = ft_strlen(str) - ft_strlen(cut);
+	aux2 = malloc(len * sizeof(char));
+	if (aux2 == NULL)
+		return (NULL);
+	cut += ft_strlen(cut);
+	str += ft_strlen(str);
+	while (len > 0)
+	{
+		if (*str == *cut)
+		{
+			str--;
+			cut--;
+		}
+		if (*str != *cut)
+		{
+			aux2[len - 1] = *str;
+			str--;
+			len--;
+		}
+	}
+	return (aux2);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char		*cut;
-	int			len1;
-	int			len2;
-	char const	*set2;
-
-	set2 = set;
-	len2 = ft_strlen(set);
-	while (*s1 == *set)
+	if (*set == 0)
 	{
-		s1++;
+		return ((char *)s1);
 	}
-	len1 = ft_strlen(s1);
-	while (s1[len1] == set2[len2])
+	s1 = start_trim(s1, set);
+	if (ft_strrchr(s1, *set) != NULL)
 	{
-		if ((len2 == 0) || (s1[len1 - 1] != set2[len2]))
-			break ;
-		len1--;
-		len2--;
+		s1 = end_trim (s1, set);
 	}
-	cut = malloc (len1 * sizeof(char));
-	if (cut == NULL)
-		return (NULL);
-	ft_memcpy(cut, s1, len1 * sizeof(char));
-	return (cut);
+	return ((char *) s1);
 }
