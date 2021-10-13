4b1f4bae9f30447b07e9f9bea10427f1d08f0d1e
Joarell <josephjoarell@gmail.com>
2021-10-13 17:09:14 -0300

    ft_strmapi, done!

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 19:12:58 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/12 21:18:42 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/13 16:21:17 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	int		len;
 	int		i;
 
+	if (!s)
+		return (NULL);
 	len = ft_strlen(s);
-	new = malloc(len * sizeof(char));
+	new = malloc((len + 1) * sizeof(char));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	i = 0;
-	while (i <= len)
+	while (i < len)
 	{
 		new[i] = (*f)(i, *s);
 		i++;
 		s++;
 	}
+	new[i] = '\0';
 	return (new);
 }
