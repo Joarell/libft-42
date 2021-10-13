2bbb83e1fae57c38169de85047ad8b1f316af899
Joarell <josephjoarell@gmail.com>
2021-10-12 21:13:18 -0300

    ft_putnbr_fd, done!

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 01:19:41 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/02 20:58:18 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/12 21:11:09 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,15 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	char	*num;
-	char	*file_d;
 
 	num = NULL;
-	file_d = NULL;
 	if (n >= INT_MIN && n <= INT_MAX)
 	{
 		num = ft_itoa(n);
-		file_d = ft_itoa(fd);
 	}
-	while (*num == *file_d && *num != '\0')
+	while (*num != '\0')
 	{
-		write(1, &*num, 1);
+		ft_putchar_fd(*num, fd);
 		num++;
-		file_d++;
 	}
 }
