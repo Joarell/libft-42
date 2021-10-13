922dcacb28b8e37e3413d4aa5e11f90b8ee528bf
Joarell <josephjoarell@gmail.com>
2021-10-13 13:39:38 -0300

    ft_memmove, done!

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 15:53:16 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/13 12:58:00 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/13 13:24:47 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,20 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	if ((!dst && !src) || !len)
 		return (dst);
 	if (dst <= src)
-		return (ft_memcpy(dst, src, len));
-	while (len > 0)
 	{
-		*(unsigned char *) dst = *(unsigned char *) src;
-		dst++;
-		src++;
-		len--;
+		return (ft_memcpy(dst, src, len));
 	}
-	dst -= zero;
+	else
+	{
+		dst = (unsigned char *)dst + len - 1;
+		src = (unsigned char *)dst + len - 1;
+		while (len > 0)
+		{
+			*(unsigned char *) dst = *(unsigned char *) src;
+			dst--;
+			src--;
+			len--;
+		}
+	}
 	return (dst);
 }
