5ea8c194a8bd301a2908c9dcd77c1673439f5b76
Joarell <josephjoarell@gmail.com>
2021-10-16 18:07:46 -0300

    ft_strlcat, done!

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,29 +6,35 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 15:53:06 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/09 14:53:24 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/16 18:04:08 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	cat_len(const char *str, size_t num)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && i < num)
+	{
+		i++;
+	}
+	return (i);
+}
+
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {	
-	if ((size <= (size_t) ft_strlen(dst)) || (size - 1 == 0))
+	size_t	d_len;
+	size_t	s_len;
+
+	d_len = cat_len(dst, size);
+	s_len = ft_strlen(src);
+	if (d_len == s_len)
 	{
-		return (*dst);
+		return (d_len + s_len);
 	}
-	if (!dst || !src)
-	{
-		return (*dst);
-	}
-	size -= ft_strlen(dst);
-	while (*dst != '\0')
-	{
-		dst++;
-	}
-	ft_memcpy(dst, src, size);
-	size = ft_strlen(dst);
-	dst[size - 1] = '\0';
-	return (*dst);
+	ft_strlcpy(&dst[d_len], src, size - d_len);
+	return (d_len + s_len);
 }
