fff60057f7df91e4eeb0cb232ea0db7724481658
Joarell <josephjoarell@gmail.com>
2021-10-17 16:18:15 -0300

    ft_strnstr, done!

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 00:12:54 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/09 17:11:24 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/17 16:12:10 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	char	*aux;
 
-	if (*little == '\0')
+	if (!*little)
 	{
 		return ((char *) big);
 	}
 	while (*big != *little)
 	{
-		if (len <= 0)
+		if (len == 0 || !*big)
 			return (NULL);
 		big++;
 		len--;
@@ -30,7 +30,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	aux = (char *) big;
 	while (*little != '\0')
 	{
-		if (len <= 0 || *big != *little)
+		if (len == 0 || *big != *little)
 		{
 			return (NULL);
 		}
