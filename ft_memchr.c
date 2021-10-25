8b816d94d50a83d6478c45d66d8a27887307f1d8
Joarell <josephjoarell@gmail.com>
2021-10-24 22:43:03 -0300

    ft_memchr, done!

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 16:20:44 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/17 17:04:54 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/24 19:57:16 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 void	*ft_memchr(const void *b, int c, size_t len)
 {
+	unsigned char	*found;
+
+	found = (unsigned char *) b;
 	if (!len)
 	{
 		return (NULL);
 	}
 	while (len--)
 	{
-		if (*(char *) b == c)
+		if (*found == (unsigned char ) c)
 		{
-			return ((unsigned char *) b);
+			return ((void *) found);
 		}
-		b++;
+		found++;
 	}
 	return (NULL);
 }
