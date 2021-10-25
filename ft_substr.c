57b1beefa6464c35eb0e0d090f5d6f27a2a84929
Joarell <josephjoarell@gmail.com>
2021-10-24 22:44:40 -0300

    ft_substr, done!

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 22:50:28 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/17 21:34:56 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/24 22:11:31 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	range = ft_strlen(s);
 	if (range < start)
+	{
 		return (ft_strdup(""));
+	}
 	s += start;
 	range = ft_strlen(s);
-	if (range <= len)
+	if (range > len)
 	{
-		copy = (char *) malloc (len * sizeof(char));
+		copy = (char *) malloc (len * sizeof(char) + 1);
 		if (copy == NULL)
 			return (NULL);
 		ft_strlcpy(copy, s, len + 1);
 		return (copy);
 	}
-	copy = (char *) malloc (range * sizeof(char));
+	copy = (char *) malloc (range * sizeof(char) + 1);
 	if (copy == NULL)
 		return (NULL);
 	ft_strlcpy(copy, s, len + 1);
