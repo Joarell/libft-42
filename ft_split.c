1e2aea96a4a465f2f0929e7fb85d5d55d6488916
Joarell <josephjoarell@gmail.com>
2021-10-27 22:44:12 -0300

    ft_split, done!

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 19:38:16 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/24 22:57:17 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/27 22:43:03 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ static	char	*next(char const *s, char c)
 		range++;
 	}
 	next = ft_substr(s, 0, range);
-	return (ft_strdup(next));
+	return (next);
 }
 
 char	**ft_split(char const *s, char c)
@@ -61,11 +61,9 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	s = ft_strtrim(s, &c);
-	storage = malloc (array_size(s, c) * sizeof(char *));
+	storage = (char **)malloc (array_size(s, c) * sizeof(char *));
 	if (storage == NULL)
-	{
 		return (NULL);
-	}
 	while (!(ft_strlen(s) == 0))
 	{
 		s = ft_strtrim(s, &c);
