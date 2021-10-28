024e7d7aa56d17e62448abdd4ef407116e752a5a
Joarell <josephjoarell@gmail.com>
2021-10-28 00:34:16 -0300

    ft_lstnew, done!

---
+++ ft_lstnew.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/27 22:55:04 by Jev               #+#    #+#             */
+/*   Updated: 2021/10/27 23:29:13 by Jev              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_e;
+
+	new_e = (t_list*) malloc (sizeof(t_list));
+	if (new_e == NULL)
+	{
+		return (NULL);
+	}
+	new_e -> content = content;
+	new_e -> next = NULL;
+	return (new_e);
+}
