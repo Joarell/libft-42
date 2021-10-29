3aa4edc4de597d8fca4498335451319906e8e217
Joarell <josephjoarell@gmail.com>
2021-10-28 21:22:33 -0300

    ft_strtrim, done!

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 23:15:50 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/28 21:10:00 by Jev              ###   ########.fr       */
+/*   Updated: 2021/10/28 21:16:13 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	}
 	s1 += trim;
 	if (!s1)
-		return (ft_strdup(""));
+		return (NULL);
 	return (end_trim(s1, set));
 }
