16b0fb0f85306ddb54b1c9dfa72f62d1fe8a46b1
Joarell <josephjoarell@gmail.com>
2021-11-05 21:56:35 -0300

    ft_strncmp, done! nov05-09-56

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: Jev <jsouza-c@student.42sp.org.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 14:18:51 by Jev               #+#    #+#             */
-/*   Updated: 2021/10/03 21:08:59 by Jev              ###   ########.fr       */
+/*   Updated: 2021/11/04 23:37:26 by Jev              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t len)
 	{
 		return (0);
 	}
-	else if (((unsigned char)*s1 == (unsigned char)*s2) && (len >= 0))
+	else if (((unsigned char)*s1 == (unsigned char)*s2) && (len))
 	{
 		s1++;
 		s2++;
